20befd5e7314563d560eb83e1fefc873b88863a7
alfset <alfset5@gmail.com>
2024-10-01 02:32:45 +0700

    fix WXFI

--- src/components/inner-page/AddLiquidityModal.tsx
+++ src/components/inner-page/AddLiquidityModal.tsx
@@ -6,7 +6,13 @@ import { ethers } from 'ethers';
 import { getTokenBalance, approveToken } from '../../utils/TokenUtils';
 import { getPoolByPairs } from '../../utils/Factory'; 
 import { provideLiquidity } from '../../utils/CoFinance';
-import { WrapNative } from '@/utils/Wrapped';
+import { WrapNative, approveAllowance } from '@/utils/Wrapped';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+import '@sweetalert2/theme-dark/dark.css';
+
+const MySwal = withReactContent(Swal);
+const Wrapped_Address = "0x28cc5edd54b1e4565317c3e0cfab551926a4cd2a"; 
 
 interface AddLiquidityModalProps {
   open: boolean;
@@ -16,6 +22,7 @@ interface AddLiquidityModalProps {
 }
 
 const AddLiquidityModal: React.FC<AddLiquidityModalProps> = ({ open, onClose, tokenA, tokenB }) => {
+  const modalRef = useRef<HTMLDivElement | null>(null);
   const [selectedTokenA, setSelectedTokenA] = useState<ImageSelect | null>(tokenA);
   const [selectedTokenB, setSelectedTokenB] = useState<ImageSelect | null>(tokenB);
   const [amountA, setAmountA] = useState<number>(0);
@@ -43,7 +50,6 @@ const AddLiquidityModal: React.FC<AddLiquidityModalProps> = ({ open, onClose, to
         const signer = await providerRef.current.getSigner();
         const accountAddress = await signer.getAddress();
         setAccount(accountAddress);
-        console.log("Connected Account:", accountAddress);
       } catch (error) {
         console.error('Error loading account and pools:', error);
       } finally {
@@ -64,13 +70,12 @@ const AddLiquidityModal: React.FC<AddLiquidityModalProps> = ({ open, onClose, to
       if (!providerRef.current || !account) return;
 
       const fetchBalance = async (token: ImageSelect | null) => {
-        if (token.value === "0x10e6414ddea2e2be27e23584c651bc0a49e11e07") {
+        if (token.value.toLowerCase() === Wrapped_Address.toLowerCase()) {
           const balance = await providerRef.current.getBalance(account);
           return ethers.formatEther(balance); 
         }
         return await getTokenBalance(providerRef.current, token.value, account);
       };
-
       const balanceA = await fetchBalance(selectedTokenA);
       const balanceB = await fetchBalance(selectedTokenB);
 
@@ -84,7 +89,6 @@ const AddLiquidityModal: React.FC<AddLiquidityModalProps> = ({ open, onClose, to
   useEffect(() => {
     const fetchPool = async () => {
       if (!providerRef.current || !selectedTokenA || !selectedTokenB) return;
-
       const poolAddress = await getPoolByPairs(providerRef.current, selectedTokenA.value, selectedTokenB.value);
       setPoolAddressFromAPI(poolAddress);
     };
@@ -95,86 +99,129 @@ const AddLiquidityModal: React.FC<AddLiquidityModalProps> = ({ open, onClose, to
   const handleConfirm = async () => {
     if (!account || !selectedTokenA || !selectedTokenB || !poolAddressFromAPI || !providerRef.current) return;
 
+    if (amountA <= 0 || amountB <= 0 || amountA > parseFloat(balanceA) || amountB > parseFloat(balanceB)) {
+      MySwal.fire({
+        icon: 'warning',
+        title: 'Invalid Amounts',
+        text: 'Please enter valid amounts for both tokens.',
+      });
+      return;
+    }
+
     try {
-      if (selectedTokenA.value === "0x10e6414ddea2e2be27e23584c651bc0a49e11e07") {
+      let txHash;
+
+      if (selectedTokenA.value.toLowerCase() === Wrapped_Address.toLowerCase()) {
         await WrapNative(providerRef.current, amountA.toString());
-        setAmountA(0); 
-      }
-      if (selectedTokenB.value === "0x10e6414ddea2e2be27e23584c651bc0a49e11e07") {
+        txHash = await approveAllowance(providerRef.current, poolAddressFromAPI, amountA.toString());
+      } else if (selectedTokenB.value.toLowerCase() === Wrapped_Address.toLowerCase()) {
         await WrapNative(providerRef.current, amountB.toString());
-        setAmountB(0); 
+        txHash = await approveAllowance(providerRef.current, poolAddressFromAPI, amountB.toString());
+      } else {
+        txHash = await approveToken(providerRef.current, selectedTokenA.value, poolAddressFromAPI, amountA.toString());
+        txHash = await approveToken(providerRef.current, selectedTokenB.value, poolAddressFromAPI, amountB.toString());
       }
-      
-      await approveToken(providerRef.current, selectedTokenA.value, poolAddressFromAPI, amountA.toString());
-      await approveToken(providerRef.current, selectedTokenB.value, poolAddressFromAPI, amountB.toString());      
-      await provideLiquidity(providerRef.current, poolAddressFromAPI, amountA.toString(), amountB.toString());
-      console.log("Liquidity added successfully:", selectedTokenA.label, selectedTokenB.label, amountA, amountB);
-      alert(`Successfully added liquidity: ${amountA} ${selectedTokenA.label} and ${amountB} ${selectedTokenB.label}`);
-      onClose(); 
+
+      const liquidityTxHash = await provideLiquidity(providerRef.current, poolAddressFromAPI, amountA.toString(), amountB.toString());
+
+      MySwal.fire({
+        icon: 'success',
+        title: 'Liquidity Added!',
+        text: `Successfully added ${amountA} ${selectedTokenA.label} and ${amountB} ${selectedTokenB.label}. Transaction Hash: ${liquidityTxHash}`,
+      });
+
+      onClose();
     } catch (error) {
       console.error('Error during liquidity provision:', error);
+      MySwal.fire({
+        icon: 'error',
+        title: 'Liquidity Provision Failed',
+        text: 'An error occurred during liquidity provision.',
+      });
     }
   };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
+        onClose();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [onClose]);
 
   if (!open) return null;
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-80 z-50">
-      <div className="bg-[#141414] p-6 rounded-xl max-w-lg w-full h-auto overflow-auto">
+      <div ref={modalRef} className="bg-[#141414] p-6 rounded-xl max-w-lg w-full">
         <h2 className="text-3xl font-semibold text-white mb-6 text-center">Add Liquidity</h2>
-        <form onSubmit={(e) => { e.preventDefault(); handleConfirm(); }}>
-          <div className="mb-4">
-            <p className="text-gray-500 text-md uppercase">Token A:</p>
-            <CustomSelectSearch
-              tokenOptions={defaultTokenOptions}
-              handleOnChange={setSelectedTokenA}
-              handleValue={selectedTokenA}
-              className="border-none hover:border-0"
-              placeholder="Choose Token A"
-            />
-            <p className="text-gray-300">Available Balance: {balanceA}</p>
-            <input 
-              type="number" 
-              value={amountA || ''}
-              onChange={(e) => setAmountA(parseFloat(e.target.value))}
-              placeholder={`Amount of ${selectedTokenA?.label || 'Token A'}`}
-              className="border border-gray-600 bg-transparent text-white p-2 rounded-xl w-full mt-2 focus:outline-none focus:border-blue-500" 
-            />
-          </div>
-          <div className="mb-4">
-            <p className="text-gray-500 text-md uppercase">Token B:</p>
-            <CustomSelectSearch
-              tokenOptions={defaultTokenOptions}
-              handleOnChange={setSelectedTokenB}
-              handleValue={selectedTokenB}
-              className="border-none hover:border-0"
-              placeholder="Choose Token B"
-            />
-            <p className="text-gray-300">Available Balance: {balanceB}</p>
-            <input 
-              type="number" 
-              value={amountB || ''}
-              onChange={(e) => setAmountB(parseFloat(e.target.value))}
-              placeholder={`Amount of ${selectedTokenB?.label || 'Token B'}`}
-              className="border border-gray-600 bg-transparent text-white p-2 rounded-xl w-full mt-2 focus:outline-none focus:border-blue-500" 
-            />
-          </div>
-          <div className="flex justify-between mt-6">
-            <button 
-              type="button" 
-              onClick={onClose} 
-              className="bg-red-600 text-white p-2 rounded-xl hover:bg-red-700"
-            >
-              Cancel
-            </button>
-            <button 
-              type="submit" 
-              className="bg-blue-600 text-white p-2 rounded-xl hover:bg-blue-700"
-            >
-              Confirm
-            </button>
-          </div>
-        </form>
+        
+        <div className="mb-4">
+          <p className="text-gray-500 text-md uppercase">Token A:</p>
+          <CustomSelectSearch
+            tokenOptions={defaultTokenOptions}
+            handleOnChange={setSelectedTokenA}
+            handleValue={selectedTokenA}
+            placeholder="Choose Token A"
+          />
+          <p className="text-gray-300">Available Balance: {balanceA}</p>
+          <input 
+            type="number" 
+            value={amountA || ''}
+            onChange={(e) => setAmountA(parseFloat(e.target.value))}
+            placeholder={`Amount of ${selectedTokenA?.label || 'Token A'}`}
+            className="border border-gray-600 bg-transparent text-white p-2 rounded-xl w-full mt-2 focus:outline-none focus:border-blue-500" 
+          />
+          <input 
+            type="range" 
+            min={0} 
+            max={parseFloat(balanceA)} 
+            value={amountA} 
+            onChange={(e) => setAmountA(parseFloat(e.target.value))}
+            className="range range-xs"
+          />
+        </div>
+
+        <div className="mb-4">
+          <p className="text-gray-500 text-md uppercase">Token B:</p>
+          <CustomSelectSearch
+            tokenOptions={defaultTokenOptions}
+            handleOnChange={setSelectedTokenB}
+            handleValue={selectedTokenB}
+            placeholder="Choose Token B"
+          />
+          <p className="text-gray-300">Available Balance: {balanceB}</p>
+          <input 
+            type="number" 
+            value={amountB || ''}
+            onChange={(e) => setAmountB(parseFloat(e.target.value))}
+            placeholder={`Amount of ${selectedTokenB?.label || 'Token B'}`}
+            className="border border-gray-600 bg-transparent text-white p-2 rounded-xl w-full mt-2 focus:outline-none focus:border-blue-500" 
+          />
+          <input 
+            type="range" 
+            min={0} 
+            max={parseFloat(balanceB)} 
+            value={amountB} 
+            onChange={(e) => setAmountB(parseFloat(e.target.value))}
+            className="range range-xs"
+          />
+        </div>
+
+        <div className="w-full text-end rounded-lg p-1 bg-[#bdc3c7]">
+          <button 
+            type="button" 
+            onClick={handleConfirm} 
+            className="btn border-0 font-thin text-lg bg-transparent hover:bg-transparent text-gray-950 w-full"
+          >
+            Add Liquidity
+          </button>
+        </div>
       </div>
     </div>
   );
