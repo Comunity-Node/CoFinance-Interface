5d2b95e6fe024a3c9c46db6d454773b2f99d3797
AlfinSug <alfinsugestiart446@gmail.com>
2024-10-04 22:34:46 +0700

    ethkl 2024 : hide for ethkl 2024

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,8 @@
 "use client";
-import React, { useState } from "react";
-import { HoveredLink } from "./ui/navbar-menu";
+import React from "react";
 import Link from "next/link";
-import { FaDiscord, FaTelegram } from 'react-icons/fa';
-import { FaXTwitter } from "react-icons/fa6";
 
 const Navbar = () => {
-    const [active, setActive] = useState<string | null>(null);
 
     return (
         <div className="navbar fixed bg-black z-50 py-5">
@@ -18,38 +14,6 @@ const Navbar = () => {
                     <li><Link href={'/portofolio'}>Portfolio</Link></li>
                     <li><Link href={'/borrow'}>Borrow</Link></li>
                     <li><Link href={'/swap'}>Trade</Link></li>
-                    <li>
-                        <details>
-                            <summary>Earn</summary>
-                            <ul className="p-2 bg-black">
-                                <div className="grid grid-cols-2 gap-8 px-2 pt-2 shadow-lg rounded-md">
-                                    <div className="space-y-2">
-                                        <p className="text-gray-500 font-normal mb-2 font-10">Pools</p>
-                                        <HoveredLink href="/staking">Staking Pools</HoveredLink>
-                                        <HoveredLink href="/pools">Pool</HoveredLink>
-                                        <HoveredLink href="/tokenstake">Token Staking</HoveredLink>
-                                    </div>
-                                    <div className="space-y-2">
-                                        <p className="text-gray-500 font-normal mb-2">Extras</p>
-                                        <HoveredLink href="/faucet">Faucet</HoveredLink>
-                                    </div>
-                                </div>
-                                <div className="flex w-full flex-col px-2">
-                                    <div className="divider divider-neutral"></div>
-                                </div>
-                                <div className="flex items-center justify-between gap-8 px-2 pb-2 bg-gradient-to-b bg-transparent shadow-lg rounded-md ">
-                                    <div className="flex items-center">
-                                        <p className="text-gray-300 font-medium">Connect With Us</p>
-                                    </div>
-                                    <div className="flex items-center space-x-4 ">
-                                        <FaDiscord className="cursor-pointer" href="" />
-                                        <FaXTwitter className="cursor-pointer" href="" />
-                                        <FaTelegram className="cursor-pointer" href="" />
-                                    </div>
-                                </div>
-                            </ul>
-                        </details>
-                    </li>
                 </ul>
             </div>
             <div className="navbar-end">
--- src/components/SwitchChain.tsx
+++ src/components/SwitchChain.tsx
@@ -6,17 +6,7 @@ import { IoIosGitNetwork } from "react-icons/io";
 
 // Define the chain options, including Solana clusters
 const chains = [
-  { value: '0x1', label: 'Ethereum', soon: false, icon: 'https://www.logo.wine/a/logo/Ethereum/Ethereum-Logo.wine.svg' },
-  { value: '0x50b', label: 'Swisstronik', soon: false, icon: 'https://s3-ap-southeast-2.amazonaws.com/www.cryptoknowmics.com/airdrops/SWTR_LOGO_SYMBOL_PNG.png' },
-  { value: '0x61', label: 'BSC', soon: false, icon: 'https://i0.wp.com/www.followchain.org/wp-content/uploads/2024/03/icons8-bnb-330.png?fit=330%2C330&ssl=1' },
-  { value: '0x1ba5', label: 'Planq', soon: false, icon: 'https://evm.planq.network/og-image.png' },
-  { value: '0x67266a7', label: 'Orichain', soon: false, icon: 'https://images.prismic.io/uphold/3f0371cb-ed69-4a01-84cb-8b9c2a814421_ORAI%402x.png?auto=compress,format' },
-  { value: '0x8274f', label: 'Scroll', soon: false, icon: 'https://global.discourse-cdn.com/standard11/uploads/scroll2/original/2X/3/3bc70fd653f9c50abbb41b7568e549535f768fcc.png' },
-  { value: '0x2105', label: 'Base', soon: false, icon: 'https://altcoinsbox.com/wp-content/uploads/2023/02/base-logo-in-blue.webp' },
-  { value: '0x103d', label: 'Cross Finance', soon: false, icon: 'https://miro.medium.com/v2/resize:fit:256/1*jTN3cYGlobHuPdnhu2lYhg.png' },
-  { value: '0x89', label: 'Polygon', soon: false, icon: 'https://cdn-icons-png.freepik.com/512/12114/12114233.png' },
-  { value: '-', label: 'Cardano', soon: true, icon: 'https://cdn4.iconfinder.com/data/icons/crypto-currency-and-coin-2/256/cardano_ada-512.png' },
-  { value: '-', label: 'Solana', soon: true, icon: 'https://cryptologos.cc/logos/solana-sol-logo.png' },
+  { value: '0x34816e', label: 'Manta Network', soon: false, icon: 'https://seeklogo.com/images/M/manta-network-manta-logo-D595CAF1F9-seeklogo.com.png' },
 ];
 
 const ChainSwitchButton: React.FC = () => {
@@ -41,7 +31,7 @@ const ChainSwitchButton: React.FC = () => {
   };
 
   return (
-    <div className="relative inline-block text-left">
+    <div className="relative inline-block text-left w-full">
       <Button
         className="mr-2"
         onClick={() => setMenuOpen(prev => !prev)} // Toggle menu visibility
@@ -59,7 +49,7 @@ const ChainSwitchButton: React.FC = () => {
       </Button>
 
       {menuOpen && (
-        <div className="absolute right-0 mt-5 w-auto p-3 text-white border border-gray-900 bg-black shadow-lg ">
+        <div className="absolute right-0 mt-5 w-64 p-3 text-white border border-gray-900 bg-black shadow-lg ">
           {chains.map((chain) => (
             <button
               key={chain.value}
