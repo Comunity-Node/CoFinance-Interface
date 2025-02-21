84db8f7a2149b8c0a92823dfe369f218ed0e17c0
Alf <42708476+alfset@users.noreply.github.com>
2025-02-22 01:57:01 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains:['images.unsplash.com', 'res.cloudinary.com',]
-    }
+  images: {
+    domains: ['images.unsplash.com', 'res.cloudinary.com'],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
